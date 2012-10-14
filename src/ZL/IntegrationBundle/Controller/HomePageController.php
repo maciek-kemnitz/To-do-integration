0fdadceece9bcd80c58a20332ee70aa57f062c54
maciek-kemnitz <maciek.kemnitz@gmail.com>
2012-10-14 19:04:52 +0200

    Login form

--- src/ZL/IntegrationBundle/Controller/HomePageController.php
+++ src/ZL/IntegrationBundle/Controller/HomePageController.php
@@ -3,14 +3,36 @@
 namespace ZL\IntegrationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use ZL\IntegrationBundle\Entity\User;
+use ZL\IntegrationBundle\Form\User\LoginUser;
 
 class HomePageController extends Controller
 {
-    public function loginAction()
+    public function loginAction(Request $request)
     {
+		$form = $this->createForm(new LoginUser(), new User());
 		
+		if ($request->getMethod() == 'POST') 
+		{
+			
+            $form->bindRequest($request);
+			
+            if ($form->isValid()) 
+			{
+			
+				$user = $form->getData();
+				
+                $session  = $this->get("session");
+				$session->set('user', $user);
+
+				$url = $this->generateUrl("main");
+				return $this->redirect($url);
+            }
+			
+		}
 		
-        return $this->render('ZLIntegrationBundle:HomePage:login.html.twig');
+        return $this->render('ZLIntegrationBundle:HomePage:login.html.twig', array('form' => $form->createView()));
     }
 	
 	public function mainAction()
--- src/ZL/IntegrationBundle/Entity/User.php
+++ src/ZL/IntegrationBundle/Entity/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zl\IntegrationBundle\Entity;
+namespace ZL\IntegrationBundle\Entity;
 
 
 /**
@@ -25,4 +25,60 @@ class User
     protected $password;
 
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     * @return User
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string 
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
 }
