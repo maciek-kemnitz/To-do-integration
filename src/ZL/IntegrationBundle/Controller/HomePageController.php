06600db90a28a9dd11cbc0edfb7c513bff52e501
maciek-kemnitz <maciek.kemnitz@gmail.com>
2013-02-24 15:51:05 +0100

    login with projects

--- src/ZL/IntegrationBundle/Controller/HomePageController.php
+++ src/ZL/IntegrationBundle/Controller/HomePageController.php
@@ -37,6 +37,35 @@ class HomePageController extends Controller
 	
 	public function mainAction()
     {
-        return $this->render('ZLIntegrationBundle:HomePage:main.html.twig');
+		$session  = $this->get("session");
+		$user = $session->get('user');
+
+		$session = curl_init();
+
+		$basecamp_url = 'https://basecamp.com/1758651/api/v1/';
+		$username = $user->getLogin();
+		$password = $user->getPassword();
+
+		curl_setopt($session, CURLOPT_URL, $basecamp_url.'projects.json');
+		curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($session, CURLOPT_HTTPGET, 1);
+		curl_setopt($session, CURLOPT_HEADER, false);
+		curl_setopt($session, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+		curl_setopt($session, CURLOPT_USERAGENT, 'MyApp (yourname@example.com)');
+		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+		curl_setopt($session,CURLOPT_USERPWD,$username . ":" . $password);
+
+
+
+		$response = curl_exec($session);
+
+		curl_close($session);
+
+		$projects = json_decode($response);
+
+		#var_dump($projects);
+
+        return $this->render('ZLIntegrationBundle:HomePage:main.html.twig', array("projects" => $projects));
     }
 }
